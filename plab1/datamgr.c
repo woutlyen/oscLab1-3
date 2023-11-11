58dcd9b9e1ef78287a1bb08bd8b86af88baf4618
Wout Lyen <woutlyen@gmail.com>
2023-11-11 14:03:15 +0100

    Added code for reading out and adding  all the sensor values of the binary file + changed the .gitignore file

--- plab1/datamgr.c
+++ plab1/datamgr.c
@@ -11,7 +11,7 @@
 typedef struct {
     uint16_t sensorID;
     uint16_t roomID;
-    float avg;
+    double avg;
     time_t last_modified;
 } element_t;
 
@@ -45,10 +45,12 @@ void datamgr_parse_sensor_files(FILE *fp_sensor_map, FILE *fp_sensor_data){
     //Malloc one element_t to temporarily store the data of one line in the room_sensor_map file
     element_t *element = (element_t*)malloc(sizeof(element_t));
 
+
     //Initialise dplist with feedback functions
     dplist_t *list = dpl_create(element_copy, element_free, element_compare);
 
-    //Add all the assigned sensorIDs to a roomID in the list
+
+    //Reading and adding all the assigned sensorIDs to roomIDs in the list
     char line[255];
     while (fgets(line, 255, fp_sensor_map) != NULL){
         sscanf(line,"%hu %hu", &(element->roomID), &(element->sensorID));
@@ -57,14 +59,35 @@ void datamgr_parse_sensor_files(FILE *fp_sensor_map, FILE *fp_sensor_data){
     }
 
 
+    //Declaration of variables in the fp_sensor_data file
     uint16_t sensorID;
     double temperature;
     time_t timestamp;
 
-    fread(&sensorID, sizeof(uint16_t), 1, fp_sensor_data);
-    fread(&temperature, sizeof(double), 1, fp_sensor_data);
-    fread(&timestamp, sizeof(time_t), 1, fp_sensor_data);
-    printf("%s", asctime(gmtime(&timestamp)));
+
+    //Reading and adding all the assigned sensor data in the list
+    while(fread(&sensorID, sizeof(uint16_t), 1, fp_sensor_data) != 0) {
+        fread(&temperature, sizeof(double), 1, fp_sensor_data);
+        fread(&timestamp, sizeof(time_t), 1, fp_sensor_data);
+
+        //Print the time_t in human language
+        //printf("%s", asctime(gmtime(&timestamp)));
+
+        for (int count = 0; count < dpl_size(list); count++) {
+
+            element_t *element_at_index = (element_t*)dpl_get_element_at_index(list, count);
+
+            if (element_at_index->sensorID == sensorID){
+                element_at_index->avg = temperature;
+                element_at_index->last_modified = timestamp;
+                break;
+            }
+
+            if (count == dpl_size(list)-1){
+                printf("%s %hu %s\n", "sensorID ", sensorID, " not found");
+            }
+        }
+    }
 }
 
 
