5d3039fa93f65c8de846f656dbd75ae40583cfa5
Wout Lyen <woutlyen@gmail.com>
2023-11-14 12:36:39 +0100

    added milestone 2 ex1

--- milestone2/ex1/sensor_db.c
+++ milestone2/ex1/sensor_db.c
@@ -4,13 +4,21 @@
 #include "sensor_db.h"
 
 FILE * open_db(char * filename, bool append){
+    FILE *file = NULL;
+    if (append){
+        file = fopen(filename, "a");
+    }
+    else{
 
+        file = fopen(filename, "a");
+    }
+    return file;
 }
 
 int insert_sensor(FILE * f, sensor_id_t id, sensor_value_t value, sensor_ts_t ts){
-    return -1;
+    return fprintf(f, "%hu, %f, %ld\n", id, value, ts);
 }
 
 int close_db(FILE * f){
-    return -1;
+    return fclose(f);
 }
