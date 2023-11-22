22d34487d93f8c03ca32be012b80a1ec4a404912
Wout Lyen <woutlyen@gmail.com>
2023-11-22 17:43:33 +0100

    removed struct

--- plab3/test_server.c
+++ plab3/test_server.c
@@ -9,11 +9,6 @@
 #include "config.h"
 #include "lib/tcpsock.h"
 
-typedef struct {
-    pthread_t id;
-    tcpsock_t *client;
-} connectionHandlerData;
-
 /**
  * Implements a sequential test server (only one connection at the same time)
  */
@@ -22,10 +17,7 @@ void *connectionHandler(void *vargp){
     int bytes, result;
     sensor_data_t data;
 
-    connectionHandlerData *connData = (connectionHandlerData*)vargp;
-    pthread_t id = connData->id;
-    tcpsock_t *client = connData->client;
-    printf("%lu\n", id);
+    tcpsock_t *client = (tcpsock_t *)vargp;
 
         printf("Incoming client connection\n");
         do {
@@ -39,7 +31,7 @@ void *connectionHandler(void *vargp){
             bytes = sizeof(data.ts);
             result = tcp_receive(client, (void *) &data.ts, &bytes);
             if ((result == TCP_NO_ERROR) && bytes) {
-                printf("thread id = %lu - sensor id = %" PRIu16 " - temperature = %g - timestamp = %ld\n", id, data.id, data.value,
+                printf("sensor id = %" PRIu16 " - temperature = %g - timestamp = %ld\n", data.id, data.value,
                        (long int) data.ts);
             }
         } while (result == TCP_NO_ERROR);
@@ -64,7 +56,6 @@ int main(int argc, char *argv[]) {
     pthread_t tid[MAX_CONN];
     tcpsock_t *server, *client[MAX_CONN];
     int conn_counter;
-    connectionHandlerData *data = malloc(sizeof(connectionHandlerData));
 
     printf("Test server is started\n");
     if (tcp_passive_open(&server, PORT) != TCP_NO_ERROR) exit(EXIT_FAILURE);
@@ -72,10 +63,8 @@ int main(int argc, char *argv[]) {
     for (conn_counter = 0; conn_counter < MAX_CONN; conn_counter++) {
         if (tcp_wait_for_connection(server, &client[conn_counter]) != TCP_NO_ERROR) exit(EXIT_FAILURE);
 
-        data->id = tid[conn_counter];
-        data->client = client[conn_counter];
 
-        pthread_create(&tid[conn_counter], NULL, connectionHandler, data);
+        pthread_create(&tid[conn_counter], NULL, connectionHandler, client[conn_counter]);
     }
 
     for (conn_counter = 0; conn_counter < MAX_CONN; conn_counter++) {
@@ -84,7 +73,6 @@ int main(int argc, char *argv[]) {
 
     if (tcp_close(&server) != TCP_NO_ERROR) exit(EXIT_FAILURE);
     printf("Test server is shutting down\n");
-    free(data);
     return 0;
 }
 
